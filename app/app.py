7e3b9670843ec479e725f5591a802e3ca323c2ea
yuvi-byte <yuvikachouhan0001@gmail.com>
2025-07-28 12:44:01 +0530

    Updated app folder with Flask code

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App Deployed via CI/CD!"
+    return "Flask App Deployed via Jenkins + docker!"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
